de7ffb009c6824be8b0cd17300fae3c68a78f206
way <1556080769@qq.com>
2025-09-24 22:47:19 +0800

    clean up

--- wayblog-web/src/main/java/com/way/wayblog/web/controller/TestController.java
+++ wayblog-web/src/main/java/com/way/wayblog/web/controller/TestController.java
@@ -4,14 +4,10 @@ import com.way.wayblog.common.aspect.ApiOperationLog;
 import com.way.wayblog.common.utils.Response;
 import com.way.wayblog.web.model.User;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.stream.Collectors;
 
 /**
  * @BelongsProject: wayblog-springboot
@@ -27,16 +23,6 @@ public class TestController {
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
     public Response test(@RequestBody @Validated User user) {
-//        // 是否存在校验错误
-//        if (bindingResult.hasErrors()) {
-//            // 获取校验不通过字段的提示信息
-//            String errorMsg = bindingResult.getFieldErrors()
-//                    .stream()
-//                    .map(FieldError::getDefaultMessage)
-//                    .collect(Collectors.joining(", "));
-//
-//            return Response.fail(errorMsg);
-//        }
         // 返参
         return Response.success();
     }
